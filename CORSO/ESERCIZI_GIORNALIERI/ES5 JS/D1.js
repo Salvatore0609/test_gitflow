c2bd8491ecba97dbe21cafab9d22aee956d72efb
Salvatore0609 <salvatoredesole1998@gmail.com>
2024-12-02 14:46:45 +0100

    Update D1.js

--- CORSO/ESERCIZI_GIORNALIERI/ES5 JS/D1.js
+++ CORSO/ESERCIZI_GIORNALIERI/ES5 JS/D1.js
@@ -12,12 +12,21 @@ REGOLE
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+/* 
+number
+string
+boolean
+null
+undefined 
+*/
 
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let myName = "Salvatore"; 
+console.log(myName)
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
@@ -25,11 +34,17 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+let num1 = 12;
+num1 += 20;
+console.log(num1)  
+
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let x = 12;
+console.log(x)
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
@@ -37,12 +52,20 @@ REGOLE
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+myName = "Desole";
+console.log(myName)
+const MYSURNAME = "Desole";
+console.log(MYSURNAME)
+/* MYSURNAME = "Solinas";
+console.log(MYSURNAME) */ //in console darà errore perchè una costante non è modificabile
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+x -= 4
+console.log(x)
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -51,3 +74,17 @@ REGOLE
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let name1 = "john"
+let name2 = "John"
+console.log(name2)
+
+let disuguaglianza = (name1 !== name2) ? "diverso" : "uguale" ;
+console.log(disuguaglianza)
+
+let uguaglianza
+if (name2.toLowerCase() === name1.toLowerCase()) {
+  uguaglianza = "uguale"
+} else {
+  uguaglianza = "diverso"
+}
+console.log(uguaglianza) //dove "John diventa john"
