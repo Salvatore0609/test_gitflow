5b49ffce227ad54ac9a673587c1edeb856edf064
Salvatore0609 <salvatoredesole1998@gmail.com>
2024-12-04 14:12:24 +0100

    Update script.js

--- CORSO/LESSONS/SETTIMANA2/L8 JS/assets/script.js
+++ CORSO/LESSONS/SETTIMANA2/L8 JS/assets/script.js
@@ -18,3 +18,25 @@ console.log(str.slice(2)); //taglia una stringa dall'indice 2 a 9
 //.substring() è simile a slice ma non accetta l'idicazione di posizione negativa.
 
 
+//.replace("parola da sostituire", "parola sostitutiva")
+console.log(str.replace("a", "X")) //sostituisce solo la prima occorrenza
+console.log(str.replaceAll("a", "X")) //sostituisce tutte le occorrenze
+
+//.toLowerCase() --> tutto minuscolo
+//.toUpperCase() --> tutto maiuscolo
+// questi hanno l'utilità di uniformare i dati per il database.
+
+//.concat() metodo alternativo al + per concatenare due stringhe, poco utilizzato
+
+//.trim() serve per eliminare gli spazi vuoti all'inizio e alla fine di uno testo
+let str1 = " Mi chiamo Salvatore, ciao a tutti "
+console.log(str1)
+console.log(str1.trim()) //evita di mettere gli spazi ad esempio per errore quando inserisci una password
+//aiuta con una pulizia dei dati
+
+//.charAt() mi permette di cercare un carattere dato un indice
+console.log(str1.charAt(5))
+
+//.split()
+let arrStr = str.split(" ")
+console.log(str1)
