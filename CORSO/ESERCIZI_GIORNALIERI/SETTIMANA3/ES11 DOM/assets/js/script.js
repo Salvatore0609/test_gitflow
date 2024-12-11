eba031a3398167f1d20216e7d96931f4519645dd
Salvatore0609 <salvatoredesole1998@gmail.com>
2024-12-11 18:13:36 +0100

    Update script.js

--- CORSO/ESERCIZI_GIORNALIERI/SETTIMANA3/ES11 DOM/assets/js/script.js
+++ CORSO/ESERCIZI_GIORNALIERI/SETTIMANA3/ES11 DOM/assets/js/script.js
@@ -79,7 +79,7 @@ function completedElement() {
     listElements.forEach(ele => {
         ele.addEventListener('click', (e) => {
             // Controlla se l'elemento è già barrato
-            if (ele.style.textDecoration === 'line-through') {
+            if (!ele.style.textDecoration === 'line-through') {
                 // Rimuovi lo stile barrato
                 ele.style.textDecoration = 'none';
             } else {
@@ -97,3 +97,81 @@ function removeUser(index) {
     userListCreated()
 }
 //RIMUOVERE CON X ------------------------------------------------------------
+
+
+
+//soluzione della collega
+/* 
+
+HTML
+----------
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>La mia To-Do List</h1>
+        <div class="input-container">
+            <input type="text" id="task-input" placeholder="Aggiungi un nuovo task">
+            <button id="add-task"> Aggiungi </button>
+        </div>
+
+        <ul id="task-list"></ul>
+    </div>
+    <script src="logica.js"></script>
+</body>
+</html>
+
+
+
+
+JS
+--------
+const taskInput = document.getElementById('task-input');
+const addButton = document.getElementById('add-task');
+const taskList = document.getElementById('task-list');
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText === '') return;
+
+    const li = document.createElement('li');
+    li.classList.add('task');
+
+    const span = document.createElement('span');
+    span.textContent = taskText;
+    span.addEventListener('click', function(e){
+     //span.style.textDecoration = 'lineThrough'
+      // console.log(e.target) 
+       e.target.style.textDecoration = 'line-through'
+       e.target.style.color = 'red'
+    })
+
+    const deleteButton = document.createElement('button');
+
+    deleteButton.style.float = 'right'
+    deleteButton.textContent = 'Elimina';
+    deleteButton.addEventListener('click', function () {
+        taskList.removeChild(li);
+    })
+
+
+    li.appendChild(span);
+    li.appendChild(deleteButton);
+    taskList.appendChild(li)
+    taskInput.value = '';
+}
+addButton.addEventListener('click', addTask);
+
+taskInput.addEventListener('keypress', function (event){
+    if (event.key === 'Enter') {
+        addTask(); 
+    }
+});
+
+ */
