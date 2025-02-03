427c6b61d2ea94b49f46bd9e153e05555a40dcaa
Salvatore0609 <salvatoredesole1998@gmail.com>
2025-02-03 16:04:22 +0100

    new

---
+++ CORSO/ESERCIZI_GIORNALIERI/SETTIMANA7/react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ButtonComponent from './components/ButtonComponent'
+
+function App() {
+  return (
+    <>
+    <ButtonComponent textButton="BOTTONE REACT"/>
+    </>
+  )
+}
+
+export default App
---
+++ CORSO/ESERCIZI_GIORNALIERI/SETTIMANA7/react-app/src/components/ButtonComponent.jsx
@@ -0,0 +1,9 @@
+const ButtonComponent = function (props) {
+  return (
+    <button>
+      {props.textButton}
+    </button>
+  );
+};
+
+export default ButtonComponent
