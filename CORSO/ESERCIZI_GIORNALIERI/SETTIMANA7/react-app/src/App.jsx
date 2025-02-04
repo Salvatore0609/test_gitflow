349e5e76144b120b52f13cc7934098aeaa7e4082
Salvatore0609 <salvatoredesole1998@gmail.com>
2025-02-04 09:19:56 +0100

    modified

--- CORSO/ESERCIZI_GIORNALIERI/SETTIMANA7/react-app/src/App.jsx
+++ CORSO/ESERCIZI_GIORNALIERI/SETTIMANA7/react-app/src/App.jsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="container d-flex flex-column align-items-center gap-5 py-5">
       <ButtonComponent bgColor="bg-danger" style={{ padding: "1rem 3rem" }} textButton="BUTTON REACT" />
-
       <ImageComponent
         imageUrl="https://images.pexels.com/photos/29913506/pexels-photo-29913506/free-photo-of-riflesso-monocromatico-di-alberi-su-un-lago-invernale.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
         alt="immagine a caso"
--- CORSO/ESERCIZI_GIORNALIERI/SETTIMANA7/react-app/src/components/ButtonComponent.jsx
+++ CORSO/ESERCIZI_GIORNALIERI/SETTIMANA7/react-app/src/components/ButtonComponent.jsx
@@ -1,16 +1,11 @@
 const ButtonComponent = function (props) {
   return (
     <>
-    <button 
-    className={props.bgColor} 
-    width={props.width} 
-    style={props.style}> 
-
-    {props.textButton} 
-
-    </button>
+      <button className={props.bgColor} width={props.width} style={props.style}>
+        {props.textButton}
+      </button>
     </>
   );
 };
 
-export default ButtonComponent
+export default ButtonComponent;
