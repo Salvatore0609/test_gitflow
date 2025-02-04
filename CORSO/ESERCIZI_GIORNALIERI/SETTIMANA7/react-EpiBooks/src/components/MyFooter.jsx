3d2c4f447ae8ac96f45ef0c2ae40621a2cdb5e1a
Salvatore0609 <salvatoredesole1998@gmail.com>
2025-02-04 17:17:23 +0100

    modified

--- CORSO/ESERCIZI_GIORNALIERI/SETTIMANA7/react-EpiBooks/src/components/MyFooter.jsx
+++ CORSO/ESERCIZI_GIORNALIERI/SETTIMANA7/react-EpiBooks/src/components/MyFooter.jsx
@@ -1,14 +1,8 @@
-import { Col, Container, Row } from "react-bootstrap";
-
 function MyFooter() {
   return (
-    <Container className="container text-center mt-3 p-4 bg-dark text-light">
-      <Row>
-        <Col>
-          <p className="mb-0">Ciao sono il footer component fatto in REACT</p>
-        </Col>
-      </Row>
-    </Container>
+    <footer className="text-center mt-3 p-4 bg-dark text-light">
+      <p className="mb-0">Ciao sono il footer component fatto in REACT</p>
+    </footer>
   );
 }
 
--- CORSO/ESERCIZI_GIORNALIERI/SETTIMANA7/react-EpiBooks/src/components/MyNav.jsx
+++ CORSO/ESERCIZI_GIORNALIERI/SETTIMANA7/react-EpiBooks/src/components/MyNav.jsx
@@ -2,17 +2,23 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 
 const MyNav = (props) => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-black">
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand className="text-white" href="#home">
           {props.title} — {props.claim}
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#">Home</Nav.Link>
-            <Nav.Link href="#">About</Nav.Link>
-            <Nav.Link href="#">Browse</Nav.Link>
+            <Nav.Link className="text-white" href="#">
+              Home
+            </Nav.Link>
+            <Nav.Link className="text-white" href="#">
+              About
+            </Nav.Link>
+            <Nav.Link className="text-white" href="#">
+              Browse
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
