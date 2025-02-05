49c3b8346cff56a98f21befff28d8c7fc1554708
Salvatore0609 <salvatoredesole1998@gmail.com>
2025-02-05 17:15:56 +0100

    m

--- CORSO/ESERCIZI_GIORNALIERI/SETTIMANA7/react-EpiBooks2/src/components/SingleBook.jsx
+++ CORSO/ESERCIZI_GIORNALIERI/SETTIMANA7/react-EpiBooks2/src/components/SingleBook.jsx
@@ -15,11 +15,11 @@ class SingleBook extends Component {
 
     return (
       <Card
-        className={`col-sm-6 col-md-4 col-lg-3 my-3 mx-2 ${this.state.selected ? "border-2 border-danger" : ""}`}
+        className={`col-sm-6 col-md-4 col-lg-3 my-3 mx-2 p-0 ${this.state.selected ? "border-2 border-danger" : ""}`}
         style={{ width: "18rem", cursor: "pointer" }}
         onClick={this.toggleSelection}
       >
-        <Card.Img variant="top" src={img} style={{ objectFit: "cover", minHeight: "420px" }} />
+        <Card.Img variant="top" src={img} style={{ objectFit: "cover", minHeight: "430px" }} />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Text>{category}</Card.Text>
