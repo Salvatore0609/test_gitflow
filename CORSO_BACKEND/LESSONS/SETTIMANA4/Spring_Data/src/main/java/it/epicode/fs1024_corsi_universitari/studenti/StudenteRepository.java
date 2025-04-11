7757868a08cc23c20e24a15650b652f6d2852cfb
Salvatore0609 <salvatoredesole1998@gmail.com>
2025-04-11 11:54:54 +0200

    new

---
+++ CORSO_BACKEND/LESSONS/SETTIMANA4/Spring_Data/src/main/java/it/epicode/fs1024_corsi_universitari/studenti/StudenteRepository.java
@@ -0,0 +1,13 @@
+package it.epicode.fs1024_corsi_universitari.studenti;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface StudenteRepository extends JpaRepository<Studente, Long> {
+    Studente findByEmail(String email);
+    Studente findByMatricola(String matricola);
+    List<Studente> findByNomeAndCognomeIgnoreCase(String nome, String cognome);
+
+}
