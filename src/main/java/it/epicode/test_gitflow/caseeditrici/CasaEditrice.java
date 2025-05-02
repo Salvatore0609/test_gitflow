43a4e87b96813c355e5a43353d84b438d9c7a1a6
Salvatore0609 <salvatoredesole1998@gmail.com>
2025-05-02 17:01:30 +0200

    add CasaEditrice class with properties for name, address, and phone

---
+++ src/main/java/it/epicode/test_gitflow/caseeditrici/CasaEditrice.java
@@ -0,0 +1,10 @@
+package it.epicode.test_gitflow.caseeditrici;
+
+import lombok.Data;
+
+@Data
+public class CasaEditrice {
+    private String nome;
+    private String indirizzo;
+    private String telefono;
+}
